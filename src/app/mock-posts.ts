86e897cff2d34485254f719f128aa6bf3ba09c80
Wilson <z5157656@ad.unsw.edu.au>
2018-09-25 10:44:40 +1000

    Revert Changes

---
+++ src/app/mock-posts.ts
@@ -0,0 +1,22 @@
+import { Post } from './post';
+
+export const POSTS: Post[] = [
+  {
+    title: 'Cook for 50-people event',
+    description: 'Cater for a wedding event with various options (vegan, vegetarian, etc..)',
+    poster_name: 'Steven',
+    task_budget: 1500,
+    task_open: true,
+    due_date: '1 April 2017',
+    location: 'Sesame Street'
+  },
+  {
+    title: 'Clean the house',
+    description: 'Clean the house before 3pm today',
+    poster_name: 'Emily',
+    task_budget: 1254,
+    task_open: true,
+    due_date: '2 April 2017',
+    location: 'Soya Street'
+  }
+];
