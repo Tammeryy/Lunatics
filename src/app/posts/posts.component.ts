d10e536f3a2bdfaeef62530375bb9e1618e853a1
Wilson <z5157656@ad.unsw.edu.au>
2018-10-23 11:01:20 +1100

    Merge branch 'wilson' of https://github.com/Tammeryy/Lunatics into wilson Fixed merge conflicts # Conflicts: #	src/app/app.module.ts #	src/app/header/header.component.html #	src/app/mock-posts.ts #	src/app/post.service.ts #	src/app/posts/posts.component.ts #	src/app/profile/profile.component.html #	src/app/task/task.component.html #	src/app/task/task.component.ts

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog, MatDialogConfig} from '@angular/material';
+
 import {POSTS} from '../mock-posts';
 import {Post} from '../post'; // dummy data
+
+import {BidTaskComponent} from '../bid-task/bid-task.component';
+
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
@@ -9,12 +14,47 @@ import {Post} from '../post'; // dummy data
 export class PostsComponent implements OnInit {
   selectedPost: Post;
   posts = POSTS;
-  constructor() { }
+
+  sortByOptions: string[] = [
+    'Price', 'Location'
+  ];
+
+  constructor(public dialog: MatDialog) {
+  }
 
   onSelect(post: Post): void {
     this.selectedPost = post;
   }
   ngOnInit() {
+  }
+
+  openBidPopup(post: Post) {
+    console.log('[BID] Title: ' + post.title + ' | Lowest Bid: ' + post.lowest_bid);
+    console.log('Bid popup called');
+    // Bid - Post title, Post's current lowest bid
+
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '30%';
+    dialogConfig.data = {
+      post_id: post.id,
+      post_title: post.title,
+      lowest_bid: post.lowest_bid
+    };
+
+    // opens a dialog box/pop-up displaying contents from PostTaskComponent's html file
+    const dialogRef = this.dialog.open(BidTaskComponent, dialogConfig);
+
+    // result refers to 'data' in [mat-dialog-close]
+    dialogRef.afterClosed().subscribe(newBid => {
+      // add to list of bids for that post
+      if (newBid && newBid.bid_offer > post.lowest_bid) post.lowest_bid = newBid.bid_offer;
+      // if (result) post.bids.push(result);
+      // for (var i in post.bids) {
+      //   console.log("[POST BID] Bidder: " +  post.bids[i].name + " | Bid Offer: " + post.bids[i].bid_offer);
+      // }
+    });
   }
 
 }
