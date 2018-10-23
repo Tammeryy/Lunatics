d0bdaca752a2ecb565721363cf5ff23d02e24a9d
Kristian Mansfield <kristianmansfield97@gmail.com>
2018-10-23 22:14:41 +1100

    Basic authentication is working. No logoff yet.

--- backend/app.py
+++ backend/app.py
@@ -17,7 +17,8 @@ api.add_resource(Login, '/login') # <username>/<password>')
     
 @app.route("/")
 def index():
-    return {"Init": "Welcome to Lunatics' chaotic backend!"}
+    print(sessions)
+    return jsonify({"Init": "Welcome to Lunatics' chaotic backend!"})
 
 # Run application
 # NOTE This needs to go last in the file
@@ -25,56 +26,3 @@ if __name__ == "__main__":
     app.run(port=2011)
 
 
-'''
-# NOTE From here below is Tammy's drafing 
-# TODO remove it
-# post - mvp - Tammy
-# get - mvp - Kris
-# search, sort, filter (2/3) - dafny then code - Lucy
-    # minimum of 2 dafny verification
-# front end - can see mvp by next week
-
-
-
-
-
-
-# request.json - getting params from frontend
-
-
-# editing json files in python?
-
-
-# passin gin params through routes bit? or a separate function?
-
-
-# createAccount - unsure about the ones that need to pass in a parameter
-
-
-# authenticate - unsure how authentications work?
-
-
-# postTask -how to edit json files?
-
-
-# getPostedTasks
-
-
-# getBiddersForTask
-@app.route("")
-def getbidders():
-    # return as json object
-
-# bidTask
-
-# getAllTasks
-@app.route("/alltasks")
-def getAllTasks():
-    return jsonify(data["posts"])
-
-
-# https://pythonspot.com/flask-web-app-with-python/
-
-
-# account to persist  - dodgy way global variable in front end since dont need to demo it
-'''
--- backend/classes.py
+++ backend/classes.py
@@ -1,4 +1,8 @@
 from flask_restful import Resource
+from flask import Flask, request, jsonify
+# from flask_restful import Api
+from classes import *
+import json
 
 # Sessions for users
 sessions = []
@@ -9,5 +13,29 @@ with open('data.json', 'r') as data_file:
     database = json.load(data_file)
 
 class Login(Resource):
-    def get(self):
-        return {'testing': 'Penis'}
+    def post(self):
+        data = json.loads(request.data)
+        if not data:
+            return {'Error': 'No input data provided'}, 400
+        try:
+            username = data['username']
+            password = data['password']
+        except:
+            return {'Error': 'Username or password not given'}, 400
+        for u in database['users']:
+            if u['username'] == username:
+                if u['password'] == password:
+                    # Generate a session token and add it to the sessions
+                    token = "Truelol"
+                    # TODO Do we want to do security? I ceebs atm.
+                    sessions.append((username, token))
+                    # Return the token
+                    return {'Success': 'Logging in', 'Token': token}
+                else:
+                    # Incorrect password
+                    return {'Error': 'Username or password incorrect'}, 400
+
+                break
+        
+        # Username not found:
+        return {'Error': 'Username or password incorrect.'}, 400
